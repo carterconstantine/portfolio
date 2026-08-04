d64cb43136c6f8572adec8c0257e9b5e74f765c5
Carter Constantine <constantine.carter09@gmail.com>
2026-08-04 02:35:40 -0400

    center svgs better (still some issues though?)

--- src/lib/Tag.svelte
+++ src/lib/Tag.svelte
@@ -11,6 +11,8 @@
     @import "../styles/theme.css";
     .tag {
         display: inline-flex;
+        align-items: center;
+        justify-content: center;
         gap: 0.375rem;
         padding: 0.5rem;
         border-radius: 9999px;
