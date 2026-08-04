ee2534b1caf65b40f16062c9145ec094790d6ddd
Carter Constantine <constantine.carter09@gmail.com>
2026-08-04 00:05:56 -0400

    add system-based light mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script>
 	import favicon from '$lib/assets/carter.png';
 	import '../app.css';
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		const media = window.matchMedia("(prefers-color-scheme: dark)");
+
+		media.addEventListener("change", (themeChange) => {
+			document.documentElement.dataset.theme = themeChange.matches ? "mocha" : "latte";
+		});
+	});
 
 	let { children } = $props();
 </script>
